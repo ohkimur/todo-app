28eca8a5e51e1e59e01dbcd8d9999b2982b1cd0a
Daniel Costrasel <ohkimur@gmail.com>
2023-02-23 22:14:30 +0200

    feat: add Checkbox custom component

--- apps/frontend/src/components/AppContainer.tsx
+++ apps/frontend/src/components/AppContainer.tsx
@@ -1,9 +1,25 @@
-import { Card } from '.'
+import { Card, Checkbox } from '.'
 
 export const AppContainer = () => {
   return (
     <div className='bg-light-grey min-h-screen flex items-center justify-center'>
-      <Card footer={<p>Ola</p>}>Ola seniora!</Card>
+      <Card footer={<p>Ola</p>}>
+        <ul className='list-none flex flex-col'>
+          <li className='flex gap-3.5'>
+            <Checkbox label='Item 1' id='check' />
+            Item 1
+          </li>
+
+          <li className='flex gap-3.5'>
+            <Checkbox label='Item 1' id='check3' />
+            Item 1
+          </li>
+          <li className='flex gap-3.5'>
+            <Checkbox label='Item 1' id='check4' />
+            Item 1
+          </li>
+        </ul>
+      </Card>
     </div>
   )
 }
---
+++ apps/frontend/src/components/Checkbox.tsx
@@ -0,0 +1,19 @@
+import { ReactComponent as CheckIcon } from '@/assets/svg/check.svg'
+
+interface ICheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+}
+
+export const Checkbox = ({ label, ...props }: ICheckboxProps) => {
+  return (
+    <div className='flex items-center gap-2'>
+      <input type='checkbox' {...props} className='hidden peer' />
+      <label
+        htmlFor={props.id}
+        className='w-[18px] h-[18px] bg-white border border-gray-400 rounded peer-checked:bg-dark-sky-blue peer-checked:border-none cursor-pointer flex items-center justify-center'
+      >
+        <CheckIcon />
+      </label>
+    </div>
+  )
+}
