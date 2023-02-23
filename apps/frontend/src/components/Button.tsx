c07fe0e883d187fa0d17c66470294e45273693f3
Daniel Costrasel <ohkimur@gmail.com>
2023-02-23 20:59:25 +0200

    chore: add styleguide colors, add Mark-Pro font as local font and let vite handle the bundling

--- apps/frontend/src/components/Button.tsx
+++ apps/frontend/src/components/Button.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const Button = ({ children }: Props) => {
   return (
-    <div className='bg-green-600 hover:scale-105 active:scale-100 hover:bg-green-500 cursor-pointer transition-all active:text-green-300 active:bg-green-700 text-green-50 px-6 py-4 rounded-md'>
+    <div className='bg-green-600 font-sans hover:scale-105 active:scale-100 hover:bg-green-500 cursor-pointer transition-all active:text-green-300 active:bg-green-700 text-green-50 px-6 py-4 rounded-md'>
       {children}
     </div>
   )
--- apps/frontend/tailwind.config.cjs
+++ apps/frontend/tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cool-grey-two': '#a1a4ad',
+        'cool-grey': '#9ea3b2',
+        'dark-blue-grey': '#1f2a4b',
+        'dark-sky-blue': '#4a77e5',
+        silver: '#d7dae0',
+        white: '#e1e1e1',
+      },
+      fontFamily: {
+        sans: ['MarkPro', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
