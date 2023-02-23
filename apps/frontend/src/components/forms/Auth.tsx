4b33a26ff20d3fb52ede585d472b173023ee5bf3
Daniel Costrasel <ohkimur@gmail.com>
2023-02-24 00:41:35 +0200

    refactor: moved zod schemas to @todos/shared

--- apps/frontend/src/components/forms/Auth.tsx
+++ apps/frontend/src/components/forms/Auth.tsx
@@ -1,22 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import { z } from 'zod'
 import { Button, Card, Input } from '..'
 
-const userSchema = z.object({
-  email: z.string().min(1, { message: 'Email is required' }).email({
-    message: 'Must be a valid email',
-  }),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be atleast 6 characters' }),
-  fullName: z
-    .string()
-    .min(3, { message: 'Full name must be at least 3 characters' }),
-})
-
-type UserSchema = z.infer<typeof userSchema>
+import { userSchema, UserSchema } from '@todos/shared'
 
 export const Auth = () => {
   const [action, setAction] = useState<'login' | 'register'>('login')
