09b688c9db3a86c0361b641088615edf13e566c7
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 00:12:23 +0200

    chore: add .env.example to apps

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -2,4 +2,5 @@ export * from './auth'
 export * from './todos'
 export * from './users'
 
-export const API_BASEPATH = 'http://localhost:3000/api/v1'
+export const API_BASEPATH =
+  import.meta.env.VITE_API_BASEPATH || 'http://localhost:3000/api/v1'
