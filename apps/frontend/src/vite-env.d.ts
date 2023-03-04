1fecbe9a7f65d673cc9587d90599418a846dd31f
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 00:13:36 +0200

    fix: vite env intellisense

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASEPATH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.svg' {
   import React = require('react')
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
