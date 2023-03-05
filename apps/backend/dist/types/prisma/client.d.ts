6abfe936ef4a6f324757ef0d500b633fc5beda5a
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 02:08:06 +0200

    chore: add vercel config

--- apps/backend/dist/types/prisma/client.d.ts
+++ apps/backend/dist/types/prisma/client.d.ts
@@ -1,1 +1,8 @@
-export declare const prisma: any
+import { PrismaClient } from '@prisma/client'
+export declare const prisma: PrismaClient<
+  import('.prisma/client').Prisma.PrismaClientOptions,
+  never,
+  | import('.prisma/client').Prisma.RejectOnNotFound
+  | import('.prisma/client').Prisma.RejectPerOperation
+  | undefined
+>
