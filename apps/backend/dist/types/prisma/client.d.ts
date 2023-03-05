64b863c9c486254f68b3a44c01b349ffd86bcfc1
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 03:28:49 +0200

    fix: prisma generate

--- apps/backend/dist/types/prisma/client.d.ts
+++ apps/backend/dist/types/prisma/client.d.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 export declare const prisma: PrismaClient<
-  import('.prisma/client').Prisma.PrismaClientOptions,
+  import('@prisma/client').Prisma.PrismaClientOptions,
   never,
-  | import('.prisma/client').Prisma.RejectOnNotFound
-  | import('.prisma/client').Prisma.RejectPerOperation
+  | import('@prisma/client').Prisma.RejectOnNotFound
+  | import('@prisma/client').Prisma.RejectPerOperation
   | undefined
 >
