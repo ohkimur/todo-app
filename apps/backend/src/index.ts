5de82498c1399f0569ee2377b10d237c3798c9ab
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 02:44:28 +0200

    chore: add vercel config

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,8 @@
+import { errorHandler, errorLogger, invalidRoute } from '@/middlewares'
+import { authRouter, todosRouter, usersRouter } from '@/routes'
+import { json, urlencoded } from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 
@@ -18,24 +23,24 @@ const corsOptions = {
   credentials: true,
 }
 
-// app.use(json())
-// app.use(cookieParser())
-// app.use(cors(corsOptions))
-// app.use(urlencoded({ extended: true }))
+app.use(json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+app.use(urlencoded({ extended: true }))
 
 app.get('/', (_req, res) => {
   res.send('Hey this is the todo-app API running 🥳')
 })
 
 // Use the routes
-// app.use('/api/v1', authRouter)
-// app.use('/api/v1', usersRouter)
-// app.use('/api/v1', todosRouter)
+app.use('/api/v1', authRouter)
+app.use('/api/v1', usersRouter)
+app.use('/api/v1', todosRouter)
 
 // Use the error handlers
-// app.use(errorLogger)
-// app.use(errorHandler)
-// app.use(invalidRoute)
+app.use(errorLogger)
+app.use(errorHandler)
+app.use(invalidRoute)
 
 app.listen(port, () => {
   console.log(`⚡️[Server]: Server is running at http://localhost:${port}`)
