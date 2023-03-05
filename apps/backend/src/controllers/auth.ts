6a177c69aa1ed731c66e3d6b2dfe69a105cfb1ef
Daniel Costrasel <ohkimur@gmail.com>
2023-03-05 04:43:16 +0200

    fix: different frontend and backend hosts

--- apps/backend/src/controllers/auth.ts
+++ apps/backend/src/controllers/auth.ts
@@ -46,6 +46,8 @@ export const login = async (
       .cookie('token', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24,
+        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
       })
       .json({
         user: {
@@ -110,6 +112,8 @@ export const register = async (
       .cookie('token', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24,
+        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
       })
       .json({
         user: {
