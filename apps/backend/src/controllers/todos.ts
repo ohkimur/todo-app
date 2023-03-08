b49e29a4ddaca117ad8ab00140fe091c5fb9f0e8
Daniel Costrasel <ohkimur@gmail.com>
2023-03-08 15:57:30 +0200

    refactor(backend): error handling

--- apps/backend/src/controllers/todos.ts
+++ apps/backend/src/controllers/todos.ts
@@ -56,66 +56,54 @@ export const getTodo = async (
 export const createTodo = async (
   req: ICustomRequest<CreateTodoSchema>,
   res: Response,
-  next: NextFunction
+  _next: NextFunction
 ) => {
-  try {
-    const { title, completed } = req.body
-    const newTodo = await prisma.todo.create({
-      data: {
-        title,
-        completed,
-        userId: req.user.id,
-      },
-    })
-    res.json(newTodo)
-  } catch (error) {
-    next(error)
-  }
+  const { title, completed } = req.body
+  const newTodo = await prisma.todo.create({
+    data: {
+      title,
+      completed,
+      userId: req.user.id,
+    },
+  })
+  res.json(newTodo)
 }
 
 export const updateTodo = async (
   req: ICustomRequest<Omit<UpdateTodoSchema, 'id'>>,
   res: Response,
-  next: NextFunction
+  _next: NextFunction
 ) => {
-  try {
-    const id = Number(req.params.id)
-    const { title, completed } = req.body
+  const id = Number(req.params.id)
+  const { title, completed } = req.body
 
-    await findTodoOrThrow(id, req.user.id)
+  await findTodoOrThrow(id, req.user.id)
 
-    const updatedTodo = await prisma.todo.update({
-      where: {
-        id,
-      },
-      data: {
-        title,
-        completed,
-      },
-    })
-    res.json(updatedTodo)
-  } catch (error) {
-    next(error)
-  }
+  const updatedTodo = await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      title,
+      completed,
+    },
+  })
+  res.json(updatedTodo)
 }
 
 export const deleteTodo = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  _next: NextFunction
 ) => {
-  try {
-    const id = Number(req.params.id)
+  const id = Number(req.params.id)
 
-    await findTodoOrThrow(id, req.user.id)
+  await findTodoOrThrow(id, req.user.id)
 
-    const deletedTodo = await prisma.todo.delete({
-      where: {
-        id,
-      },
-    })
-    res.json(deletedTodo)
-  } catch (error) {
-    next(error)
-  }
+  const deletedTodo = await prisma.todo.delete({
+    where: {
+      id,
+    },
+  })
+  res.json(deletedTodo)
 }
